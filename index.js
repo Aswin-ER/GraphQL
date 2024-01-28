d119e353940af24db59c7378f139b15b85484815
Aswin Rajan <aswinachu3333@gmail.com>
2024-01-29 00:01:15 +0530

    Implemented resolver functions related to fetching single data

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 //db
-import db from'./_db.js';
+import db from './_db.js';
 
 //types
-import  { typeDefs } from './schema.js'
+import { typeDefs } from './schema.js'
 
 const resolvers = {
     Query: {
         games() {
             return db.games
         },
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id);
+        },
         authors() {
             return db.authors
         },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id);
+        },
         reviews() {
             return db.reviews
+        },
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id);
         }
     }
 }
@@ -25,7 +34,7 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers
-})  
+})
 
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
